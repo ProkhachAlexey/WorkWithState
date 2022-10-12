513fc6ded65130c5e8f04f07ed467e01f72bd269
Alexey Prokhach <asprokhach68@gmail.com>
2022-10-12 19:50:34 +0300

    Added viewModel

--- app/src/main/java/com/prokhach/workwithstate/ExampleState4.kt
+++ app/src/main/java/com/prokhach/workwithstate/ExampleState4.kt
@@ -1,17 +1,58 @@
 package com.prokhach.workwithstate
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.prokhach.workwithstate.databinding.ActivityMainBinding
+import java.util.Observer
 
 class ExampleState4 : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
+
+    private val viewModel: ExampleState4ViewModel by viewModels<ExampleState4ViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
-//        initViews()
+        initViews()
+
+        viewModel.initState(
+            savedInstanceState?.getParcelable(KEY_STATE) ?: ExampleState4ViewModel.State(
+                counterValue = 0,
+                counterTextColor = ContextCompat.getColor(this, R.color.purple_700),
+                counterIsVisible = true
+            )
+        )
+
+        viewModel.state.observe(this, androidx.lifecycle.Observer {
+            renderState(it)
+        })
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putParcelable(KEY_STATE, viewModel.state.value)
+    }
+
+    private fun initViews() = with(binding) {
+        incrementButton.setOnClickListener { viewModel.increment() }
+        randomColorButton.setOnClickListener { viewModel.setRandomColor() }
+        switchVisibilityButton.setOnClickListener { viewModel.switchVisibility() }
+    }
+
+    private fun renderState(state: ExampleState4ViewModel.State) = with(binding.counterTextView) {
+        text = state.counterValue.toString()
+        setTextColor(state.counterTextColor)
+        visibility = if (state.counterIsVisible) View.VISIBLE else View.INVISIBLE
+    }
+
+    private companion object {
+
+        const val KEY_STATE = "STATE"
     }
 }
--- app/src/main/java/com/prokhach/workwithstate/ExampleState4ViewModel.kt
+++ app/src/main/java/com/prokhach/workwithstate/ExampleState4ViewModel.kt
@@ -1,8 +1,52 @@
 package com.prokhach.workwithstate
 
+import android.graphics.Color
+import android.os.Parcelable
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.android.parcel.Parcelize
+import kotlin.random.Random
 
 class ExampleState4ViewModel : ViewModel() {
 
+    private val stateLiveData = MutableLiveData<State>()
+    val state: LiveData<State>
+        get() = stateLiveData
 
+    fun initState(state: State) {
+        stateLiveData.value = state
+    }
+
+    fun increment() {
+        val oldState = stateLiveData.value
+        stateLiveData.value = oldState?.copy(
+            counterValue = oldState.counterValue + 1
+        )
+    }
+
+    fun setRandomColor() {
+        val oldState = stateLiveData.value
+        stateLiveData.value = oldState?.copy(
+            counterTextColor = Color.rgb(
+                Random.nextInt(256),
+                Random.nextInt(256),
+                Random.nextInt(256)
+            )
+        )
+    }
+
+    fun switchVisibility() {
+        val oldState = stateLiveData.value
+        stateLiveData.value = oldState?.copy(
+            counterIsVisible = !oldState.counterIsVisible
+        )
+    }
+
+    @Parcelize
+    data class State(
+        var counterValue: Int,
+        var counterTextColor: Int,
+        var counterIsVisible: Boolean
+    ): Parcelable
 }
